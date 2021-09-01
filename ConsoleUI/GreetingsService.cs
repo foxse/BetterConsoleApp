770e7c583d3ea299b6c1376c7d274d0fd9f3e7dc
soxser <soxser@gmail.com>
2021-09-01 13:26:19 +0300

    Add project files

---
+++ ConsoleUI/GreetingsService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+//DI, Serilog, Settings
+
+namespace ConsoleUI
+{
+    public class GreetingsService : IGreetingsService
+    {
+        private readonly ILogger<GreetingsService> _log;
+        private readonly IConfiguration _config;
+
+        public GreetingsService(ILogger<GreetingsService> log, IConfiguration config)
+        {
+            _log = log;
+            _config = config;
+        }
+
+        public void Run()
+        {
+            while (_config.GetValue<int>("LoopTimes") > 0)
+            {
+                for (int i = 0; i < _config.GetValue<int>("LoopTimes"); i++)
+                {
+                    _log.LogInformation("Run number {runNumber}", i);
+                }
+                _log.LogInformation("Waiting for 2 seconds");
+                Thread.Sleep(2000);
+            }
+            _log.LogInformation("Exiting GreetingsService");
+
+        }
+    }
+}
---
+++ ConsoleUI/IGreetingsService.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsoleUI
+{
+    public interface IGreetingsService
+    {
+        void Run();
+    }
+}
